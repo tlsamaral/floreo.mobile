0dba7707fc6750e6d7b852a5ad634829a21e8c0f
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-04-01 21:24:36 -0300

    index page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from 'react-native'
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1">
+      <Text className="font-bold">Edit app/index.tsx to edit this screen.</Text>
     </View>
   )
 }
