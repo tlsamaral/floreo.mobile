ecb80b9616533293901db7d7a24fc1771bd4bd80
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-04-01 21:24:54 -0300

    feat: create layout root

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,20 @@
-import { Stack } from 'expo-router'
+import { ImageBackground, View } from 'react-native'
+import '../styles/global.css'
+
+import { Slot } from 'expo-router'
+import Contants from 'expo-constants'
+
+const statusBarHeight = Contants.statusBarHeight
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />
+  return (
+    <ImageBackground
+      source={require('../assets/auth-root.png')}
+      style={{ flex: 1 }}
+    >
+      <View className="flex-1 px-8" style={{ paddingTop: statusBarHeight }}>
+        <Slot />
+      </View>
+    </ImageBackground>
+  )
 }
