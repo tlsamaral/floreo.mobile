0bb5b16b78c8961f558fafb6327655d20d0447d1
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-04-01 21:24:08 -0300

    feat: add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+import { colors as colorsSystem } from './constants/colors'
+
+import colorsTailwind from 'tailwindcss/colors'
+
+const colors = {
+  ...colorsSystem,
+  ...colorsTailwind,
+}
+
 module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
+    colors,
     extend: {},
   },
   plugins: [],
