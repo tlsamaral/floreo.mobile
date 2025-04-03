861b1c1a57837e85acd2b38bef041fe95ae8561a
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-04-02 23:03:12 -0300

    feat: create index page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,33 @@
-import { Text, View } from 'react-native'
+import { ImageBackground, View } from 'react-native'
+import { Asset } from 'expo-asset'
+import { Button } from '../components/button'
+import { useRouter } from 'expo-router'
+
+const backgroundImage = Asset.fromModule(require('../assets/auth-root.png')).uri
 
 export default function Index() {
+  const router = useRouter()
   return (
-    <View className="flex-1">
-      <Text className="font-bold">Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <ImageBackground
+      source={{ uri: backgroundImage }}
+      style={{ flex: 1 }}
+      resizeMode="cover"
+    >
+      <View className="flex-1 flex-col justify-end items-end pb-[150px] px-6">
+        <View className="flex-col gap-4 w-full">
+          <Button
+            text="Vamos começar"
+            className="rounded-full"
+            variant="secondary"
+            onPress={() => router.push('/auth/sign-in')}
+          />
+          <Button
+            text="Acessar minha conta"
+            className="rounded-full"
+            onPress={() => router.push('/auth/sign-in')}
+          />
+        </View>
+      </View>
+    </ImageBackground>
   )
 }
