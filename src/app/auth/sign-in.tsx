6cc1d5113582c554391b8579ce915ea5e44c6fd6
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-04-03 21:21:49 -0300

    add sign in page

--- src/app/auth/sign-in.tsx
+++ src/app/auth/sign-in.tsx
@@ -1,10 +1,10 @@
-import { Input } from '@/src/components/input'
+import { Input } from '../../components/ui/Input'
 import { View } from 'react-native'
 
 export default function SignIn() {
   return (
-    <View className="flex-1">
-      <Input placeholder="Digite seu email" variant="light-blur" />
+    <View className="flex-1 justify-center items-center">
+      <Input placeholder="Digite seu email" />
     </View>
   )
 }
