35428828e589a29bf5d616092455397bff402188
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-04-03 21:33:12 -0300

    feat: add buttons

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,8 @@
 import { ImageBackground, View } from 'react-native'
 import { Asset } from 'expo-asset'
-import { Button } from '../components/button'
+import { Button } from '../components/ui/Button'
 import { useRouter } from 'expo-router'
+import { Text } from '@/components/ui/Text'
 
 const backgroundImage = Asset.fromModule(require('../assets/auth-root.png')).uri
 
@@ -16,16 +17,18 @@ export default function Index() {
       <View className="flex-1 flex-col justify-end items-end pb-[150px] px-6">
         <View className="flex-col gap-4 w-full">
           <Button
-            text="Vamos começar"
             className="rounded-full"
             variant="secondary"
             onPress={() => router.push('/auth/sign-in')}
-          />
+          >
+            <Text>Destructive</Text>
+          </Button>
           <Button
-            text="Acessar minha conta"
             className="rounded-full"
             onPress={() => router.push('/auth/sign-in')}
-          />
+          >
+            <Text>Destructive</Text>
+          </Button>
         </View>
       </View>
     </ImageBackground>
