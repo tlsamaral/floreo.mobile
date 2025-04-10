3ea69e36866b667e84ceecb42d1e311b883fc526
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-04-09 21:34:19 -0300

    remove image

--- src/app/auth/_layout.tsx
+++ src/app/auth/_layout.tsx
@@ -1,7 +1,7 @@
 import Contants from 'expo-constants'
 import { Asset } from 'expo-asset'
 import { Slot } from 'expo-router'
-import { ImageBackground, View } from 'react-native'
+import { View } from 'react-native'
 
 const statusBarHeight = Contants.statusBarHeight
 const backgroundImage = Asset.fromModule(
@@ -10,14 +10,14 @@ const backgroundImage = Asset.fromModule(
 
 export default function AuthLayout() {
   return (
-    <ImageBackground
-      source={{ uri: backgroundImage }}
-      style={{ flex: 1 }}
-      resizeMode="cover"
-    >
-      <View className="px-6">
-        <Slot />
-      </View>
-    </ImageBackground>
+    // <ImageBackground
+    //   source={{ uri: backgroundImage }}
+    //   style={{ flex: 1 }}
+    //   resizeMode="cover"
+    // >
+    <View className="px-10 flex-1 bg-brand-900">
+      <Slot />
+    </View>
+    // </ImageBackground>
   )
 }
