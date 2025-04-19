40e229b64e78fdd4fbfce1dc2955d84ed58128c8
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-04-19 17:44:27 -0300

    feat: add default plants page

---
+++ src/app/(app)/(tabs)/plants.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function PlantsScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-second-50">
+      <Text className="text-lg">Página Plants</Text>
+    </View>
+  )
+}
