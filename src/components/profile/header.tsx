4263e466fa7b3e9fd16f1638c8a899285b645248
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-04-20 11:18:11 -0300

    feat: add header to profile sections

---
+++ src/components/profile/header.tsx
@@ -0,0 +1,25 @@
+import { View, TouchableOpacity } from 'react-native'
+import { useRouter } from 'expo-router'
+import { Text } from '../ui/Text'
+import { ChevronLeft } from 'lucide-react-native'
+
+export default function ProfileHeader({ title }: { title: string }) {
+  const router = useRouter()
+
+  return (
+    <View className="h-16 bg-gray-100 px-6 pt-3 flex-row items-center justify-between">
+      <TouchableOpacity
+        className="size-7 rounded-full bg-brand-900 flex justify-center items-center"
+        onPress={() => router.replace('/(app)/(tabs)/profile')}
+      >
+        <ChevronLeft size={18} color="#FDFDF9" />
+      </TouchableOpacity>
+
+      <Text className="text-2xl font-bold text-brand-900 flex-1 text-center">
+        {title}
+      </Text>
+
+      <View className="w-6" />
+    </View>
+  )
+}
