e0f8094ccbdc0d6cabff72b377f1fb00906c2207
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-04-20 11:20:20 -0300

    change profile data screen

---
+++ src/app/(app)/(tabs)/profile/change-profile.tsx
@@ -0,0 +1,11 @@
+import { Text } from '@/components/ui/Text'
+import { View } from 'react-native'
+
+export default function ChangeProfile() {
+  return (
+    <View className="flex-1 items-center justify-center px-7">
+      <Text className="text-lg text-brand-900">Alterar Perfil</Text>
+      {/* Aqui você pode adicionar o formulário para alterar o perfil */}
+    </View>
+  )
+}
