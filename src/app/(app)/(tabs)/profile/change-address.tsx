9ef8a9a9116cedad82bffc6925ba0c51de9ff02f
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-04-20 11:19:40 -0300

    change address screen

---
+++ src/app/(app)/(tabs)/profile/change-address.tsx
@@ -0,0 +1,11 @@
+import { Text } from '@/components/ui/Text'
+import { View } from 'react-native'
+
+export default function ChangeAddress() {
+  return (
+    <View className="flex-1 items-center justify-center px-7">
+      <Text className="text-lg text-brand-900">Alterar endereço</Text>
+      {/* Aqui você pode adicionar o formulário para alterar o perfil */}
+    </View>
+  )
+}
