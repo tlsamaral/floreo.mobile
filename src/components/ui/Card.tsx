c5fc96e6b7b2affa2b3d5564e593d257f9b9fc2e
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-04-23 21:27:15 -0300

    feat: ui card

---
+++ src/components/ui/Card.tsx
@@ -0,0 +1,80 @@
+import { type ComponentPropsWithoutRef, forwardRef } from 'react'
+import { Text, View } from 'react-native'
+import { TextClassContext } from './Text'
+import type { TextRef, ViewRef } from '@rn-primitives/types'
+import { cn } from '@/lib/utils'
+
+const Card = forwardRef<ViewRef, ComponentPropsWithoutRef<typeof View>>(
+  ({ className, ...props }, ref) => (
+    <View
+      ref={ref}
+      className={cn(
+        'rounded-lg border border-border bg-card shadow-sm shadow-foreground/10',
+        className,
+      )}
+      {...props}
+    />
+  ),
+)
+Card.displayName = 'Card'
+
+const CardHeader = forwardRef<ViewRef, ComponentPropsWithoutRef<typeof View>>(
+  ({ className, ...props }, ref) => (
+    <View
+      ref={ref}
+      className={cn('flex flex-col space-y-1.5 p-6', className)}
+      {...props}
+    />
+  ),
+)
+CardHeader.displayName = 'CardHeader'
+
+const CardTitle = forwardRef<TextRef, ComponentPropsWithoutRef<typeof Text>>(
+  ({ className, ...props }, ref) => (
+    <Text
+      role="heading"
+      aria-level={3}
+      ref={ref}
+      className={cn(
+        'text-2xl text-card-foreground font-semibold leading-none tracking-tight',
+        className,
+      )}
+      {...props}
+    />
+  ),
+)
+CardTitle.displayName = 'CardTitle'
+
+const CardDescription = forwardRef<
+  TextRef,
+  React.ComponentPropsWithoutRef<typeof Text>
+>(({ className, ...props }, ref) => (
+  <Text
+    ref={ref}
+    className={cn('text-sm text-muted-foreground', className)}
+    {...props}
+  />
+))
+CardDescription.displayName = 'CardDescription'
+
+const CardContent = forwardRef<ViewRef, ComponentPropsWithoutRef<typeof View>>(
+  ({ className, ...props }, ref) => (
+    <TextClassContext.Provider value="text-card-foreground">
+      <View ref={ref} className={cn('p-6 pt-0', className)} {...props} />
+    </TextClassContext.Provider>
+  ),
+)
+CardContent.displayName = 'CardContent'
+
+const CardFooter = forwardRef<ViewRef, ComponentPropsWithoutRef<typeof View>>(
+  ({ className, ...props }, ref) => (
+    <View
+      ref={ref}
+      className={cn('flex flex-row items-center p-6 pt-0', className)}
+      {...props}
+    />
+  ),
+)
+CardFooter.displayName = 'CardFooter'
+
+export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
