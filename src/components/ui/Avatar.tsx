cedadf730bfedcb5b6a3e56b6f4085f2dea86643
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-04-23 21:27:29 -0300

    ui avatar

---
+++ src/components/ui/Avatar.tsx
@@ -0,0 +1,55 @@
+import {
+  forwardRef,
+  type ElementRef,
+  type ComponentPropsWithoutRef,
+} from 'react'
+import * as AvatarPrimitive from '@rn-primitives/avatar'
+import { cn } from '@/lib/utils'
+
+const AvatarPrimitiveRoot = AvatarPrimitive.Root
+const AvatarPrimitiveImage = AvatarPrimitive.Image
+const AvatarPrimitiveFallback = AvatarPrimitive.Fallback
+
+const Avatar = forwardRef<
+  ElementRef<typeof AvatarPrimitiveRoot>,
+  ComponentPropsWithoutRef<typeof AvatarPrimitiveRoot>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitiveRoot
+    ref={ref}
+    className={cn(
+      'relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full',
+      className,
+    )}
+    {...props}
+  />
+))
+Avatar.displayName = AvatarPrimitiveRoot.displayName
+
+const AvatarImage = forwardRef<
+  ElementRef<typeof AvatarPrimitiveImage>,
+  ComponentPropsWithoutRef<typeof AvatarPrimitiveImage>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitiveImage
+    ref={ref}
+    className={cn('aspect-square h-full w-full', className)}
+    {...props}
+  />
+))
+AvatarImage.displayName = AvatarPrimitiveImage.displayName
+
+const AvatarFallback = forwardRef<
+  ElementRef<typeof AvatarPrimitiveFallback>,
+  ComponentPropsWithoutRef<typeof AvatarPrimitiveFallback>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitiveFallback
+    ref={ref}
+    className={cn(
+      'flex h-full w-full items-center justify-center rounded-full bg-muted',
+      className,
+    )}
+    {...props}
+  />
+))
+AvatarFallback.displayName = AvatarPrimitiveFallback.displayName
+
+export { Avatar, AvatarFallback, AvatarImage }
