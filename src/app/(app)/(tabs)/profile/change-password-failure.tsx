1c9f0db3341efc6803183c5386cfc0c7155b9548
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-04-24 22:49:25 -0300

    feat: add ui alert to sinalize that user use social login

--- src/app/(app)/(tabs)/profile/change-password-failure.tsx
+++ src/app/(app)/(tabs)/profile/change-password-failure.tsx
@@ -1,11 +1,21 @@
 import { TextInput } from '@/components/input'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/Alert'
 import { Button } from '@/components/ui/Button'
 import { Text } from '@/components/ui/Text'
+import { CircleAlert } from 'lucide-react-native'
 import { View } from 'react-native'
 
 export default function ChangePassword() {
   return (
     <View className="flex-1 items-center px-7 gap-5 py-10">
+      <Alert icon={CircleAlert} variant="warning" className="max-w-xl">
+        <AlertTitle className="text-[#FFCF50]">Ops 😓.</AlertTitle>
+        <AlertDescription className="text-[#FFCF50]">
+          Este usuário utiliza o sistema com login social, não é possível
+          alterar a senha.
+        </AlertDescription>
+      </Alert>
+
       <View className="flex-col gap-1 w-full">
         <Text className="text-lg text-brand-900">Senha atual</Text>
         <TextInput
