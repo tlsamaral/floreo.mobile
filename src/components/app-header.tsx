3cb21e59a7ef1e0a45c1a72320edf88f58361e08
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-04-27 12:41:44 -0300

    feat: add app header with button to back

---
+++ src/components/app-header.tsx
@@ -0,0 +1,35 @@
+import { View, TouchableOpacity } from 'react-native'
+import { type Href, useRouter } from 'expo-router'
+import { Text } from './ui/Text'
+import { ChevronLeft } from 'lucide-react-native'
+
+import Contants from 'expo-constants'
+
+const statusBarHeight = Contants.statusBarHeight
+
+export function AppHeader({
+  title,
+  returnTo,
+}: { title: string; returnTo: Href }) {
+  const router = useRouter()
+
+  return (
+    <View
+      className="h-16 bg-gray-100 px-6 flex-row items-center justify-between"
+      style={{ paddingTop: statusBarHeight + 8 }}
+    >
+      <TouchableOpacity
+        className="size-7 rounded-full bg-brand-900 flex justify-center items-center"
+        onPress={() => router.replace(returnTo)}
+      >
+        <ChevronLeft size={18} color="#FDFDF9" />
+      </TouchableOpacity>
+
+      <Text className="text-2xl font-bold text-brand-900 flex-1 text-center">
+        {title}
+      </Text>
+
+      <View className="w-6" />
+    </View>
+  )
+}
