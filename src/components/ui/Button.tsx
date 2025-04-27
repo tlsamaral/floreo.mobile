27b52b60066f13c81d38a16f56c9d9e2612122a9
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-04-27 12:42:13 -0300

    feat: add variant to button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
         default: 'bg-primary web:hover:opacity-90 active:opacity-90',
         destructive: 'bg-destructive web:hover:opacity-90 active:opacity-90',
         outline:
-          'border border-input bg-background web:hover:bg-accent web:hover:text-accent-foreground active:bg-accent',
+          'border border-input rounded-full border-brand-900 web:hover:bg-accent web:hover:text-accent-foreground active:bg-accent',
         secondary: 'bg-secondary web:hover:opacity-80 active:opacity-80',
         ghost:
           'web:hover:bg-accent web:hover:text-accent-foreground active:bg-accent',
@@ -47,7 +47,7 @@ const buttonTextVariants = cva(
       variant: {
         default: 'text-primary-foreground',
         destructive: 'text-destructive-foreground',
-        outline: 'group-active:text-accent-foreground',
+        outline: 'text-brand-900',
         secondary:
           'text-secondary-foreground group-active:text-secondary-foreground',
         ghost: 'group-active:text-accent-foreground',
