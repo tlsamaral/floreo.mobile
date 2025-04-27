f3cf9262f3f9c99488376d0e075093a40c6ab663
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-04-27 13:53:25 -0300

    feat: create layout for engine screens

---
+++ src/app/(app)/(tabs)/engines/_layout.tsx
@@ -0,0 +1,38 @@
+// app/(tabs)/engines/_layout.tsx
+import { Stack } from 'expo-router'
+import { AppHeader } from '@/components/app-header'
+
+export default function EnginesStackLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="empty"
+        options={{
+          headerShown: false,
+          header: () => (
+            <AppHeader
+              title="Adicionar Floreo"
+              returnTo="/engines/new-engine"
+            />
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="[id]"
+        options={{
+          headerShown: false,
+
+          header: () => (
+            <AppHeader title="Detalhes do Floreo" returnTo="/engines" />
+          ),
+        }}
+      />
+    </Stack>
+  )
+}
