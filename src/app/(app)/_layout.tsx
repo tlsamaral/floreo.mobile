9d2a59e1dca2632c4afec0ea8c1cefb9aa353aaf
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-05-02 19:41:14 -0300

    feat: new validate when header is show

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -4,11 +4,16 @@ import { View } from 'react-native'
 
 export default function AppLayout() {
   const pathname = usePathname()
+
   const isProfileRoute = pathname.startsWith('/profile')
+
+  const isNewEnginesRoute = pathname.startsWith('/engines/new-engine')
+
+  const canViewHeader = !isProfileRoute && !isNewEnginesRoute
 
   return (
     <View className="flex-1 bg-second-50">
-      {!isProfileRoute && <Header />}
+      {canViewHeader && <Header />}
       <Slot />
     </View>
   )
