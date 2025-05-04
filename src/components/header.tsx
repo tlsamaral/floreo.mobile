5ffd35964da86965ba9e49976e70491fcf91613a
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-05-04 13:25:50 -0300

    feat: modify header layout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -20,25 +20,24 @@ export function getGreetingWithEmoji(): string {
 
 export function Header() {
   return (
-    <View
-      className="w-[85%] mx-auto flex-row items-center justify-between py-3 border-b border-b-second-600 bg-second-50"
-      style={{ paddingTop: statusBarHeight }}
-    >
-      <View className="flex-row items-center gap-2">
-        <Image source={require('../assets/avatar.png')} />
-        <View>
-          <Text className="font-medium text-2xl text-brand-900 m-0 ">
-            {getGreetingWithEmoji()}
-          </Text>
-          <Text className="text-lg text-brand-900 leading-none">
-            Felipe antonio
-          </Text>
+    <View className="px-8 bg-second-50" style={{ paddingTop: statusBarHeight }}>
+      <View className="pb-3 border-b border-b-second-600 flex-row items-center justify-between">
+        <View className="flex-row items-center gap-2">
+          <Image source={require('../assets/avatar.png')} />
+          <View>
+            <Text className="font-medium text-2xl text-brand-900 m-0 ">
+              {getGreetingWithEmoji()}
+            </Text>
+            <Text className="text-lg text-brand-900 leading-none">
+              Felipe antonio
+            </Text>
+          </View>
         </View>
-      </View>
 
-      <Pressable className="w-10 h-10 items-center justify-center rounded-full bg-tertiary-100">
-        <Bell size={20} color={'#3C683A'} />
-      </Pressable>
+        <Pressable className="w-10 h-10 items-center justify-center rounded-full bg-tertiary-100">
+          <Bell size={20} color={'#3C683A'} />
+        </Pressable>
+      </View>
     </View>
   )
 }
