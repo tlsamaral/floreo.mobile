11a189ec98fb00eda775755f5549814ffd0b14fc
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-05-10 12:59:25 -0300

    floreo card

--- src/components/floreo-card.tsx
+++ src/components/floreo-card.tsx
@@ -2,25 +2,28 @@ import { ChevronRight, Sprout } from 'lucide-react-native'
 import { Card, CardContent, CardHeader, CardTitle } from './ui/Card'
 import { View } from 'react-native'
 import { Text } from './ui/Text'
+import { Link } from 'expo-router'
 
 export function FloreoCard() {
   return (
-    <Card className="bg-second-200 w-full border border-brand-900 flex-row">
-      <View className="flex-1">
-        <CardHeader className="flex-row items-center gap-2 pb-2">
-          <Sprout size={24} color="#504120" />
-          <CardTitle className="text-second-900">FLR-2020</CardTitle>
-        </CardHeader>
-        <CardContent className="flex-row items-center gap-2">
-          <View className="size-2 rounded-full bg-danger-500" />
-          <Text className="text-base font-medium text-second-700">
-            Desativado
-          </Text>
-        </CardContent>
-      </View>
-      <View className="fitems-end justify-center pr-4">
-        <ChevronRight size={24} color="#504120" />
-      </View>
-    </Card>
+    <Link href="/(app)/(tabs)/engines/1">
+      <Card className="bg-second-200 w-full border border-brand-900 flex-row">
+        <View className="flex-1">
+          <CardHeader className="flex-row items-center gap-2 pb-2">
+            <Sprout size={24} color="#504120" />
+            <CardTitle className="text-second-900">FLR-2020</CardTitle>
+          </CardHeader>
+          <CardContent className="flex-row items-center gap-2">
+            <View className="size-2 rounded-full bg-danger-500" />
+            <Text className="text-base font-medium text-second-700">
+              Desativado
+            </Text>
+          </CardContent>
+        </View>
+        <View className="fitems-end justify-center pr-4">
+          <ChevronRight size={24} color="#504120" />
+        </View>
+      </Card>
+    </Link>
   )
 }
