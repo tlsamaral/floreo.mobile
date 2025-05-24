cd26440adc21ab317471e152534d68f0f691c867
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-05-24 13:57:56 -0300

    redirect user

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -1,6 +1,13 @@
 import { Header } from '@/components/header'
-import { Slot, usePathname } from 'expo-router'
+import { useAuth } from '@/contexts/auth-context'
+import { Asset } from 'expo-asset'
+import { router, Slot, usePathname } from 'expo-router'
+import { useEffect } from 'react'
 import { View } from 'react-native'
+
+const backgroundImage = Asset.fromModule(
+  require('../../../assets/images/background.png'),
+).uri
 
 export default function AppLayout() {
   const pathname = usePathname()
@@ -11,8 +18,19 @@ export default function AppLayout() {
 
   const canViewHeader = !isProfileRoute && !isNewEnginesRoute
 
+  const { isAuthenticated, loading } = useAuth()
+
+  useEffect(() => {
+    if (!loading && !isAuthenticated) {
+      router.replace('/auth/sign-in')
+    }
+  }, [isAuthenticated, loading])
+
   return (
-    <View className="flex-1 bg-second-50">
+    <View
+      className="flex-1 bg-second-50"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
       {canViewHeader && <Header />}
       <Slot />
     </View>
