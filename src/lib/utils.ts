c0fd0f5902c6f4c020464fbb5f9b1c30485764d1
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-05-25 16:18:16 -0300

    feat: create function to generate a random username

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -16,3 +16,14 @@ export function getInitials(name: string): string {
 
   return (first + last).toUpperCase()
 }
+
+export function generateUsername(name: string): string {
+  if (!name) return ''
+
+  const nameParts = name.trim().toLowerCase().split(' ')
+  const first = nameParts[0]
+  const last = nameParts.length > 1 ? nameParts[nameParts.length - 1] : ''
+  const randomNum = Math.floor(100 + Math.random() * 900) // Gera número entre 100 e 999
+
+  return `${first}.${last}${randomNum}`
+}
