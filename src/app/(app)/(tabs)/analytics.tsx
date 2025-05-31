3f31be4835742c30e235c4c2e8310568921fcb1c
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-05-31 20:12:15 -0300

    remove

--- src/app/(app)/(tabs)/analytics.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native'
-
-export default function AnalyticsScreen() {
-  return (
-    <View className="flex-1 items-center justify-center bg-second-50">
-      <Text className="text-lg">Página Analytics</Text>
-    </View>
-  )
-}
