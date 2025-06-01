a96975cb6ea87cb201c6b4294b04d69887dd9dbd
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-06-01 14:46:34 -0300

    feat: use real data

--- src/components/plant-card.tsx
+++ src/components/plant-card.tsx
@@ -3,32 +3,40 @@ import { Card, CardContent, CardHeader } from './ui/Card'
 import { Image, View } from 'react-native'
 import { Text } from './ui/Text'
 import { Link } from 'expo-router'
+import dayjs from 'dayjs'
 
-export function PlantCard() {
+interface PlantCardProps {
+  name: string
+  friendlyName: string
+  plantingDate: string
+  imageUri: string
+}
+
+export function PlantCard({ name, friendlyName, plantingDate, imageUri }: PlantCardProps) {
   return (
     <Link href="/(app)/(tabs)/plants/1">
       <Card className="bg-second-200 w-full border border-brand-900 flex-row px-4 items-center">
         <View>
           <Image
-            source={require('../../assets/images/plant-base-image.png')}
+            source={{ uri: imageUri }}
             className="size-[60px] rounded-lg"
           />
         </View>
         <View className="flex-1">
           <CardHeader className="flex-row items-center gap-1 pb-2">
-            <View className="flex-row gap-1 max-w-[255px] overflow-hidden">
+            <View className="flex-row items-baseline gap-1 max-w-[255px] overflow-hidden">
               <Text className="text-2xl font-medium text-second-900">
-                Samabaia
+                {friendlyName}
               </Text>
-              {/* <Text className="text-base font-medium text-second-700 truncate">
-                Nephrolepis exaltata
-              </Text> */}
+              <Text className="text-base font-medium text-second-700 truncate">
+                ,{' '}{name}
+              </Text>
             </View>
           </CardHeader>
           <CardContent className="flex-row items-center gap-2">
-            <View className="size-2 rounded-full bg-danger-500" />
+            <View className="size-2 rounded-full bg-green-500" />
             <Text className="text-base font-medium text-second-700">
-              Desativado
+              Plantada em: {dayjs(plantingDate).format('DD/MM/YYYY')}
             </Text>
           </CardContent>
         </View>
